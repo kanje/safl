fd55c83bb19c0df840cad542401cc48e9bffdd8e
Antons Jeļkins <anton.jelkin@gmail.com>
2018-03-20 22:15:52 +0100

    Continued with the documentation.

--- lib/include/safl/Future.h
+++ lib/include/safl/Future.h
@@ -12,24 +12,92 @@ namespace safl {
 /**
  * @brief The Future.
  */
-template<typename ValueType>
-class Future final
-        : public detail::FutureBase<ValueType>
+template<typename tValueType>
+class Future
+        : public detail::FutureNtBase
 {
-    using detail::FutureBase<ValueType>::FutureBase;
+public:
+    using ValueType = tValueType;
+    using ContextType = detail::ContextBase<ValueType>;
 
 public:
-    const ValueType &value() const noexcept
+    /**
+     * @brief Specify a continuation.
+     */
+    template<typename tFunc>
+    auto then(tFunc &&f) noexcept
+    {
+        return m_ctx->then(std::forward<tFunc>(f));
+    }
+
+    /**
+     * @brief Specify an error handler.
+     */
+    template<typename tFunc>
+    auto &onError(tFunc &&f) noexcept
+    {
+        m_ctx->onError(std::forward<tFunc>(f));
+        return *static_cast<Future<tValueType>*>(this);
+    }
+
+    /**
+     * @brief Send a message to an asynchronous operation.
+     */
+    template<typename tMessage>
+    void sendMessage(tMessage &&msg) noexcept
+    {
+        m_ctx->sendMessage(std::forward<tMessage>(msg));
+    }
+
+    /**
+     * @brief Check if this @future is ready.
+     */
+    bool isReady() const noexcept
+    {
+        return m_ctx->isReady();
+    }
+
+    /**
+     * @brief Get the value.
+     */
+    template<typename xValueType = tValueType,
+             typename = std::enable_if_t<!std::is_void<xValueType>::value>>
+    const xValueType &value() const noexcept
     {
         return this->m_ctx->value();
     }
-};
 
-template<>
-class Future<void>
-        : public detail::FutureBase<void>
-{
-    using FutureBase<void>::FutureBase;
+public:
+    Future(ContextType *ctx)
+        : m_ctx(ctx)
+    {
+        m_ctx->attachFuture();
+    }
+
+    Future(Future &&other)
+        : m_ctx(other.m_ctx)
+    {
+        other.m_ctx = nullptr;
+    }
+
+    ~Future()
+    {
+        if ( m_ctx ) {
+            m_ctx->detachFuture();
+        }
+    }
+
+    [[ gnu::warn_unused_result ]]
+    ContextType *makeShadowOf(ContextType *ctx) noexcept
+    {
+        ContextType *tmp = m_ctx;
+        m_ctx->makeShadowOf(ctx);
+        m_ctx = nullptr;
+        return tmp;
+    }
+
+protected:
+    ContextType *m_ctx;
 };
 
 using BrokePromise = detail::BrokenPromise;
